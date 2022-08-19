cc8f6c181ef06c7892954d89b887ad9c9726b157
José Luis Vilches <jose.luis.comvilches@gmail.com>
2022-08-19 00:21:58 -0300

    Iniciamos el proyecto, agregamos routing y configuración de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//*** Importamos las clases para trabajar con firestone ***//
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+//**********************************************************************//
+
+//*** Importamos la configuración de firebase ***//
+import { environment } from 'src/environments/environment';
+//**********************************************************************//
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
