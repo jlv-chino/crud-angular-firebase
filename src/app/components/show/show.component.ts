2309c25344646286dc6dd0483de94bef5bedaa6d
José Luis Vilches <jose.luis.comvilches@gmail.com>
2022-08-30 01:05:37 -0300

    Traemos los datos desde la DB y los mostramos

--- src/app/components/show/show.component.ts
+++ src/app/components/show/show.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+/*** Importamos el modelo ***/
+import { Post } from '../../post.model';
+
+/*** Importamos el servicio ***/
+import { PostService } from '../../post.service';
+
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
-
-  constructor() { }
+  Posts: Post[]
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.getPosts().subscribe( (res) =>{
+      this.Posts = res.map( (e) =>{
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as Post)
+        };
+      });
+    });
   }
+  
+  deleteRow = (post) => this.postService.deletePost(post); 
 
 }
