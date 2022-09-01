859d313c36afc51fe8d28378d6a2e2a57ef5c8e6
José Luis Vilches <jose.luis.comvilches@gmail.com>
2022-09-01 01:34:16 -0300

    Completamos el componente Edit

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PostService } from 'src/app/post.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  public editForm: FormGroup;
+  postRef: any;
+  
+  constructor(
+    public postService: PostService,
+    public formBuilder: FormBuilder,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.editForm = this.formBuilder.group({
+      title: [''],
+      content: [''],
+      author: ['']
+    })
+   }
 
   ngOnInit(): void {
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.postService.getPostById(id).subscribe( res => {
+      this.postRef = res;
+      this.editForm = this.formBuilder.group({
+        title: [this.postRef.title],
+        content: [this.postRef.content],
+        author: [this.postRef.author]
+      }) 
+    })
+  }
+
+  onSubmit(){
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.postService.updatePost(this.editForm.value, id);
+    this.router.navigate(['']);
   }
 
 }
